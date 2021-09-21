4dd523c0cf2a6c62c1de5d7b263246680b5e4e29
Karina <rejobo6557@gmail.com>
2021-09-21 10:42:56 +0600

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,9 @@
 import os
+
 from pathlib import Path
+from decouple import config, Csv
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(xrfqwtnivwv@if%+lsiqk9ttd&vn($(dw&luos==(3glo%l_2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
 
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
